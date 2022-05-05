05a215fc1efb02974312bc3f91c471ef73934af3
tttrang152k <98499142+tttrang152k@users.noreply.github.com>
2022-05-05 14:24:20 -0700

    modified open and remove files

--- json_merger.py
+++ json_merger.py
@@ -4,9 +4,9 @@ import os
 # Call this function recursively until only one file is left in the indexes directory
 def mergeFiles(f1: str, f2: str) -> None:
     # Open and load both files
-    with open(f1) as f:
+    with open(os.path.join("indexes",f1)) as f:
         data1 = json.load(f)
-    with open(f2) as f:
+    with open(os.path.join("indexes",f2)) as f:
         data2 = json.load(f)
     # Iterate through key-value pairs and add to the second dictionary
     for k,v in data1.items():
@@ -20,7 +20,7 @@ def mergeFiles(f1: str, f2: str) -> None:
                     data2[k]["locations"].append(i)
                 data2[k]["frequency"] += data1[k]["frequency"]
     # Save the data to file 1
-    with open(f1, "w") as f:
+    with open(os.path.join("indexes",f1), "w") as f:
         json.dump(data2, f)
     # Delete file 2
-    os.remove(f2)
+    os.remove(os.path.join("indexes",f2))
