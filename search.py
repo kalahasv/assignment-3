ae27c0f08e5def0db4d7ab7ac5ac8b6efce2c9be
Vikasni Kalahasthi <kalahasv@uci.edu>
2022-05-13 11:35:46 -0700

    "added function to return urls from indexes"

--- search.py
+++ search.py
@@ -1,6 +1,9 @@
 import os
 import json
+from posixpath import splitext
 from nltk.stem.porter import *
+from urllib.parse import urlparse
+from pprint import pprint
 
 # intersection function based on the pseudocode from class notes
 def intersection(x: list, y: list) -> list:
@@ -17,6 +20,35 @@ def intersection(x: list, y: list) -> list:
         else:
             cur_y_index += 1
     return answer
+
+
+def find_urls(docPath,index_list) -> list: #returns a list of urls associated with the given fids 
+    urls = []
+    counter = 1 #count the current index of where we are in the files
+   
+    for root, dirs, files in os.walk(docPath): #go through the files again 
+        dirs.sort() #sort dirs so they are in the same order every time
+        for page in files:
+            
+            with open(os.path.join(root, page)) as json_file:
+                data = json.load(json_file)
+            
+            extension = splitext(urlparse(data["url"]).path)[1] #gets the extension, esentially just making sure we're traversing the same .jsons in the same order
+            if(extension != '.txt' and extension != '.php'):
+                #print("Hi we're here at the file")
+                #print(counter)
+                #print("Index list:",index_list[0])
+                if str(counter) in index_list[0]:
+                   #print("counter was found in url")
+                   urls.append(data["url"])
+                   
+                
+                counter+=1
+            if(len(urls) == len(index_list[0])):
+                pprint(urls)
+                return urls
+    pprint(urls)
+    return urls
 
 # Steps:
 # 1. Search for EACH search term from the inverted index
@@ -51,6 +83,14 @@ if __name__ == "__main__":
         while len(docs) > 1:
             same = intersection(docs.pop(), docs.pop())
             docs.append(same)
-        print(docs[0])
+        print(docs[0]) #note: remove later
     else:
-        print(docs[0])
+        print(docs[0]) #note: remove later
+
+
+
+    #find the urls of the list of docs 
+    docPath ="DEV_TEST"
+    urls = find_urls(docPath,docs)
+    
+   
