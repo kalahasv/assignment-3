83943cfd1314f30fde2be855d88db0d89ef3ccff
Tim Chen <timchen1901@gmail.com>
2022-05-15 19:23:40 -0700

    Full search suggestions implementation

--- webserver.py
+++ webserver.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template, jsonify
 import mysql.connector
 
+
+sql = mysql.connector.connect(
+        host="localhost",
+        user="search",
+        password="",
+        database="search_engine",
+        pool_name="sqlPool",
+        pool_size=3
+    )
+query = sql.cursor()
 app = Flask(__name__)
+
 
 @app.route("/")
 def homepage():
     return render_template("home.html")
 
-@app.route("/query")
-def query():
-    return jsonify({"hello": "world"})
+@app.route("/query/")
+def no_query():
+    return jsonify(list())
+
+
+@app.route("/query/<input>")
+def query_db(input):
+    query.execute("SELECT * FROM terms WHERE content LIKE %s LIMIT 5", (input + "%",))
+    result = query.fetchall()
+    sql.commit()
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
