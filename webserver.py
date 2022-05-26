b47833b1042a6d06d44f6c3dd248314ff4d87c44
Tim Chen <timchen1901@gmail.com>
2022-05-26 15:14:19 -0700

    bug fixes

--- webserver.py
+++ webserver.py
@@ -13,25 +13,28 @@ sql = mysql.connector.connect(
         pool_name="sqlPool",
         pool_size=3
     )
-query = sql.cursor()
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 r = Rake()
 
+# Suppresses 404s by returning a blank page instead
 @app.errorhandler(404)
 def invalid_path(error):
     return ""
 
+# Renders the homepage
 @app.route("/")
 def homepage():
     return render_template("home.html")
 
+# Flask is garbage so this route is used for empty queries
 @app.route("/query/")
 def no_query():
     return jsonify(list())
 
+# Renders the ads creation page
 @app.route("/ads", methods=["GET"])
 def get_ads():
     if not session.get("loggedin"):
@@ -39,12 +42,14 @@ def get_ads():
     else:
         return render_template("createads.html")
 
+# Adds the ad to the database
 @app.route("/ads", methods=["POST"])
 def create_ads():
     if not session.get("loggedin"):
         return "Log in first!", 400
-    else:
+    elif request.values.get("title") and request.values.get("body") and request.values.get("site") and request.values.get("cpc") and request.values.get("keywords[0][tag]"):
         try:
+            query = sql.cursor()
             query.execute("INSERT INTO ads (title, content, url, user_id) VALUES (%s, %s, %s, %s)", (request.values.get("title"), request.values.get("body"), request.values.get("site"), session["uid"]))
             l = query.lastrowid
             print(request.values.get("keywords[0][tag]"))
@@ -55,20 +60,28 @@ def create_ads():
                 query.execute("INSERT INTO ads_keywords (ad_id, word, cpc) VALUES (%s, %s, %s)", (l,request.values.get("keywords[" + str(counter) + "][tag]"), request.values.get("cpc")))
                 counter += 1
             sql.commit()
+            query.close()
             return "Thanks for submitting the ad!"
         except:
             return "bad input!", 400
+    else:
+        return "bad input!", 400
 
+# Fetch search suggestions as the user types, if they stop typing for some reason in the middle
 @app.route("/query/<input>")
 def query_db(input):
     input = input.split(" ")
+    query = sql.cursor()
     query.execute("SELECT * FROM terms WHERE content LIKE %s LIMIT 5", ("%".join(input) + "%",))
     result = query.fetchall()
     sql.commit()
+    query.close()
     return jsonify(result)
 
+# Render the search results page along with potential ads that are associated with it
 @app.route("/search/<input>")
 def search_page(input):
+    query = sql.cursor()
     d = list()
     a = list()
     r.extract_keywords_from_text(input)
@@ -79,7 +92,6 @@ def search_page(input):
         sql.commit()
         if results:
             for result in results:
-                print(result)
                 query.execute("SELECT balance FROM users, ads, ads_keywords WHERE users.id = ads.user_id AND ads.id = ads_keywords.ad_id AND ads_keywords.word = %s LIMIT 1", (result[1],))
                 res = query.fetchone()
                 sql.commit()
@@ -97,10 +109,22 @@ def search_page(input):
         d = search.searchEngineData(urls)
         if len(a) > 0:
             d.insert(0, a)
+    if session.get("loggedin") == True:
+        query.execute("SELECT * FROM users_searches WHERE search = %s AND user_id = %s", (input, session["uid"]))
+        res2 = query.fetchone()
+        if res2:
+            query.execute("UPDATE users_searches SET count = count + 1 WHERE search = %s AND user_id = %s", (input, session["uid"]))
+            sql.commit()
+        else:
+            query.execute("INSERT INTO users_searches (user_id, search, count) VALUES (%s, %s, 1)", (session["uid"], input))
+            sql.commit()
+    query.close()
     return render_template("searchresults.html", data=d)
 
+# Renders the web page by fetching the JSON file associated with it, if its a result, OR if its an ad, redirect the user to the ad page
 @app.route("/render/<path:input>")
 def render_page(input):
+    query = sql.cursor()
     if input[:3] == "ad:":
         frags = input.split(":")
         if len(frags) < 3:
@@ -108,56 +132,88 @@ def render_page(input):
         try:
             query.execute("SELECT users.id, ads.url, ads_keywords.cpc FROM users,ads,ads_keywords WHERE ads.id = %s AND ads.user_id = users.id AND ads.id = ads_keywords.ad_id AND ads_keywords.word = %s LIMIT 1", (frags[1],frags[2]))
             res = query.fetchone()
-            print(res)
             query.execute("UPDATE users SET balance = balance - %s WHERE id = %s", (res[2], res[0]))
             sql.commit()
+            # Record ad clicks for potential analytics data
+            if session.get("loggedin") == True:
+                query.execute("SELECT * FROM users_clicks WHERE click = %s AND user_id = %s", (input, session["uid"]))
+                res2 = query.fetchone()
+                if res2:
+                    query.execute("UPDATE users_clicks SET count = count + 1 WHERE click = %s AND user_id = %s", (input, session["uid"]))
+                    sql.commit()
+                else:
+                    query.execute("INSERT INTO users_clicks (user_id, click, count, type) VALUES (%s, %s, 1, 'Ad')", (session["uid"], input))
+                    sql.commit()
+            query.close()
             return redirect(res[1])
         except:
+            query.close()
             return 'Ad Owner ran out of money', 400
     try:
+        # Record clicks to manipulate the next time the user enters the same search query
+        if session.get("loggedin") == True:
+            query.execute("SELECT * FROM users_clicks WHERE click = %s AND user_id = %s", (input, session["uid"]))
+            res = query.fetchone()
+            if res:
+                query.execute("UPDATE users_clicks SET count = count + 1 WHERE click = %s AND user_id = %s", (input, session["uid"]))
+                sql.commit()
+            else:
+                query.execute("INSERT INTO users_clicks (user_id, click, count, type) VALUES (%s, %s, 1, 'Result')", (session["uid"], input))
+                sql.commit()
         with open(input) as f:
             data = json.load(f)
+        query.close()
         return data["content"]
     except:
+        query.close()
         return "Invalid file path"
 
+# Renders the login page
 @app.route("/login", methods=["GET"])
 def login():
     if session.get("loggedin"):
         return redirect("/")
     return render_template("login.html")
 
+# Renders the registration page
 @app.route("/register", methods=["GET"])
 def register():
     if session.get("loggedin"):
         return redirect("/")
     return render_template("register.html")
 
+# Basic login function with no password hashing
 @app.route("/login", methods=["POST"])
 def login_auth():
     if session.get("loggedin"):
         return 'Already logged in!', 400
     if request.form.get("email") and request.form.get("password"):
+        query = sql.cursor()
         query.execute("SELECT * FROM users WHERE email = %s AND password = %s", (request.form.get("email"), request.form.get("password")))
         result = query.fetchone()
         if result:
             session["loggedin"] = True
             session["uid"] = result[0]
             session["user"] = request.form.get("email")
+            query.close()
             return 'Success!'
         else:
+            query.close()
             return 'Invalid credentials!', 401
     else:
         return 'Invalid credentials!', 401
 
+# Basic registration function with no password hashing
 @app.route("/register", methods=["POST"])
 def register_auth():
     if session.get("loggedin"):
         return 'Already logged in!', 401
     if request.form.get("email") and request.form.get("password"):
+        query = sql.cursor()
         query.execute("SELECT * FROM users WHERE email = %s", (request.form.get("email"),))
         result = query.fetchone()
         if result:
+            query.close()
             return 'User exists!', 401
         else:
             query.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (request.form.get("email"), request.form.get("password")))
@@ -165,10 +221,12 @@ def register_auth():
             session["loggedin"] = True
             session["uid"] = query.lastrowid
             session["user"] = request.form.get("email")
+            query.close()
             return 'Success!'
     else:
         return 'Invalid request!', 401
 
+# Logs the user out by destroying the session
 @app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
