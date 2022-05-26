8bea40600fc859184cd0487aa098ac89e5950df0
Victor Guan <vguan12@uci.edu>
2022-05-25 18:26:03 -0700

    Changed search so that the relevant index is loaded for each query term

--- search.py
+++ search.py
@@ -18,24 +18,9 @@ URL = 'urlmap.json'
 with open(URL_PATH) as f:
     urlpath = json.load(f)
 
-# opens all the files produced in the splitter
-def open_split_files():
+misc_ind = ""
+letter_indexes = {}
 
-    json_splitter.splitFile('index1.json')
-
-    # misc_index is the loaded misc index from splitter
-    global misc_index
-    misc_index = json.load(open(os.path.join("split_indexes", "misc" + ".json")))
-    
-    # letter_indexes is the list of each letter's loaded index from json
-    global letter_indexes
-    letter_indexes = {}
-    
-    # open the split index for each letter
-    alphabet = list(string.ascii_lowercase)
-    for letter in alphabet:
-        if os.path.exists("split_indexes/" + letter + ".json"):
-            letter_indexes[letter] = json.load(open(os.path.join("split_indexes", letter + ".json")))
 
 # intersection function based on the pseudocode from class notes
 def intersection(x: list, y: list) -> list:
@@ -72,9 +57,15 @@ def find_urls(index_list) -> list: #returns a list of urls associated with the g
 def buildDocList(inputs: list) -> list:
     docs_list = []
     stemmer = PorterStemmer()
+
+    # makes sure the variables defined in __main__ can be used here 
+    # (had a problem with using misc_ind inside this function)
+    global misc_ind 
+    global letter_indexes
     # For each individual word, find the entry in the index, should implement boolean logic here too.
     
     for query in inputs:
+
         docs = [] # [ [key,value] ]
         #print("Current query: ",query)
         stemmed = stemmer.stem(query)
@@ -82,17 +73,23 @@ def buildDocList(inputs: list) -> list:
         #pprint(index.keys())
 
         first_char = stemmed[0]
+       
         #print(first_char)
 
         # If first character in the word is a letter, find associated word in stemmed file
         if first_char in list(string.ascii_lowercase):
+            if (letter_indexes[first_char] == "") and os.path.exists("split_indexes/" + first_char + ".json"):
+                letter_indexes[first_char] = json.load(open(os.path.join("split_indexes", first_char + ".json")))
             stemmed_index = letter_indexes[first_char]
         # Else, find in the miscellaneous file
         else:
-            stemmed_index = misc_index
+            if (misc_ind == "") and os.path.exists("split_indexes/" + "misc.json"):
+                misc_ind = json.load(open(os.path.join("split_indexes", "misc" + ".json")))
+            stemmed_index = misc_ind
             
         # If the word is in the file, find the doc ids and the number of appearances
         if stemmed in stemmed_index:
+            
             for key, value in stemmed_index[stemmed]['locations'].items():
                 docs.append([key, value])
                 
@@ -140,10 +137,15 @@ def searchEngineData(l: list) -> list:
 # 3. Find intersection between the retrieved sets of documents
 
 if __name__ == "__main__":
-    # open all the split files
-    open_split_files()
+    #open_split_files()
 
     while True:
+        # Create empty split indexes
+        alphabet = list(string.ascii_lowercase)
+        for letter in alphabet:
+            letter_indexes[letter] = ""
+        misc_index = ""
+            
         # Array containing each word of the query
         queries = list(input("Search Query: ").split())
         # The timer begins when the query is beginning to be processed
